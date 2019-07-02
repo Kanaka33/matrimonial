4488671caaf20624add63b892eb2adc6993ff74c
Kanaka33 <50393708+Kanaka33@users.noreply.github.com>
2019-07-02 16:48:37 +0530

    Functional changes

--- index.php
+++ index.php
@@ -1,56 +1,74 @@
-<section class="sct-color-1">
-    <div class="container-fluid no-padding">
-        <div class="row row-no-padding">
-            <?php $slider_status = $this->db->get_where('frontend_settings', array('type' => 'slider_status'))->row()->value;
-            $home_members_status = $this->db->get_where('frontend_settings', array('type' => 'home_members_status'))->row()->value;
-            $home_parallax_status = $this->db->get_where('frontend_settings', array('type' => 'home_parallax_status'))->row()->value;
-            $home_stories_status = $this->db->get_where('frontend_settings', array('type' => 'home_stories_status'))->row()->value;
-            $home_plans_status = $this->db->get_where('frontend_settings', array('type' => 'home_plans_status'))->row()->value;
-            $home_contact_status = $this->db->get_where('frontend_settings', array('type' => 'home_contact_status'))->row()->value;
-            $slider_position = $this->db->get_where('frontend_settings', array('type' => 'slider_position'))->row()->value;
-            if($slider_status=='yes'){
-                $home_search_style = $this->db->get_where('frontend_settings', array('type' => 'home_search_style'))->row()->value;
-				if (!empty($this->session->userdata['member_id'])) { 
-                if ($home_search_style == '2') {
-                    if($slider_position=='left'){
-                        include_once 'slider_2.php';
-						  
-							include_once 'search.php';
-						 
-                    } elseif($slider_position=='right'){
-                        // if (!empty($this->session->userdata['member_id'])) {
-							include_once 'search.php';
-						 //} 
-                        include_once 'slider_2.php';
-                    }
-                } 
-			}else  {
-					//if ($home_search_style == '1') {
-                    include_once 'slider.php';
-                }
-            }
-            ?>
+<style>
+    @media (max-width: 576px) {
+        .listing-image {
+            height: 330px !important;
+        }
+    }
+
+</style>
+<section class="page-title page-title--style-1">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-12 text-center">
+                <h2 class="heading heading-3 strong-400 mb-0"><?php echo translate('active_members')?></h2>
+            </div>
         </div>
     </div>
 </section>
-<?php
-    if($home_members_status=='yes'){
-       // include_once'premium_members.php';
-    }
-    if($home_parallax_status=='yes'){
-        include_once'parallax.php';
-    }
-    if($home_stories_status=='yes'){
-        //include_once'happy_stories.php';
-    }
-    if($home_plans_status=='yes'){
-        //include_once'packages.php';
-    }
-    if($home_contact_status=='yes'){
-        include_once'contact.php';
-    }
-?>
-<script src="<?=base_url()?>template/front/js/jquery.inputmask.bundle.min.js"></script>
+<section class="slice sct-color-1">
+    <div class="container">
+        <div class="row">
+			<?php  $advance_search_position = $this->db->get_where('frontend_settings', array('type' => 'advance_search_position'))->row()->value;
+
+            if($advance_search_position=='left'){
+                include_once'filter.php';
+
+			?>
+            <div class="col-lg-8">
+
+                <input type="hidden" id="member_type" value="<?php if(!empty($member_type)){echo $member_type;}?>">
+
+                <div class="block-wrapper" id="result">
+                    <!-- Loads List Data with Ajax Pagination -->
+                </div>
+                <div id="pagination" style="float: right;">
+                    <!-- Loads Ajax Pagination Links -->
+                </div>
+            </div>
+            <?php }else{ ?>
+             <div class="col-lg-8">
+
+                <input type="hidden" id="member_type" value="<?php if(!empty($member_type)){echo $member_type;}?>">
+
+                <div class="block-wrapper" id="result">
+                    <!-- Loads List Data with Ajax Pagination -->
+                </div>
+                <div id="pagination" style="float: right;">
+                    <!-- Loads Ajax Pagination Links -->
+                </div>
+            </div>
+            <?php include_once'filter.php'; } ?>
+        </div>
+    </div>
+</section>
+<script>
+    $(document).ready(function(){
+        $("#filter_aged_from").change(function(){
+            var from = parseInt($("#filter_aged_from").val());
+            var to = parseInt($("#filter_aged_to").val());
+            if (to < from) {
+                var to = $("#filter_aged_to").val(from);
+            }
+        });
+        $("#filter_aged_to").change(function(){
+            var from = parseInt($("#filter_aged_from").val());
+            var to = parseInt($("#filter_aged_to").val());
+            if (to < from) {
+                var to = $("#filter_aged_to").val(from);
+            }
+        });
+    });
+</script>
 <script>
     $(document).ready(function(){
         $(".height_mask").inputmask({
@@ -66,23 +84,158 @@
 </script>
 <script>
     $(document).ready(function(){
-        $("#aged_from").change(function(){
-            var from = parseInt($("#aged_from").val());
-            var to = parseInt($("#aged_to").val());
-            if (to < from) {
-                var to = $("#aged_to").val(from);
-            }
-        });
-        $("#aged_to").change(function(){
-            var from = parseInt($("#aged_from").val());
-            var to = parseInt($("#aged_to").val());
-            if (to < from) {
-                var to = $("#aged_to").val(from);
-            }
-        });
+        var home_search = "<?=$home_search?>";
+        var home_religion = "<?=$home_religion?>";
+
+        var home_caste = "<?=$home_caste?>";
+        var home_sub_caste = "<?=$home_sub_caste?>";
+        if (home_caste != null && home_caste != "") {
+            $.ajax({
+                url: "<?=base_url()?>home/get_dropdown_by_id_caste/caste/religion_id/"+home_religion+"/"+home_caste, // form action url
+                type: 'POST', // form submit method get/post
+                dataType: 'html', // request type html/json/xml
+                cache       : false,
+                contentType : false,
+                processData : false,
+                success: function(data) {
+
+                    $(".s_caste").html(data);
+                    $("#sub_caste").css("display", "none");
+
+                },
+                error: function(e) {
+                    console.log(e)
+                }
+            });
+        }
+        if (home_sub_caste != null && home_sub_caste != "") {
+
+             $.ajax({
+                url: "<?=base_url()?>home/get_dropdown_by_id_sub_caste/sub_caste/caste_id/"+home_caste+"/"+home_sub_caste, // form action url
+                type: 'POST', // form submit method get/post
+                dataType: 'html', // request type html/json/xml
+                cache       : false,
+                contentType : false,
+                processData : false,
+
+                success: function (data) {
+
+                    if(data == false ){
+
+                       $("#sub_caste").css("display", "none");
+                    } else {
+                      $(".s_sub_caste").html(data);
+                       $("#sub_caste").css("display", "block");
+                    };
+               },
+                error: function(e) {
+                    console.log(e)
+                }
+            });
+        }
+        if (home_search == "false") {
+            filter_members('0');
+        }
+        if (home_search == "true") {
+            filter_members('0', 'search');
+        }
+
     });
 
-     $(".s_religion").change(function(){
+    function filter_members(page, type){
+		var formdata = false;
+       if (type == 'search'){
+           var url = '<?php echo base_url(); ?>home/ajax_member_list/'+page+'/'+type;
+		   formdata = $('#filter_form').serialize();
+       }else {
+           var member_type = "";
+           if ($("#member_type").val() != "") {
+               member_type = $("#member_type").val();
+           }
+           var url = '<?php echo base_url(); ?>home/ajax_member_list/'+page+'/'+member_type;
+       }
+       var place = $('#result');
+       $(".btn-back-to-top").click();
+       $.ajax({
+           url: url, // form action url
+           type: 'POST', // form submit method get/post
+           dataType: 'html', // request type html/json/xml
+           data: formdata ? formdata : null, // serialize form data
+           cache       : false,
+           beforeSend: function() {
+               place.html("");
+               place.html("<div class='text-center pt-5 pb-5' id='payment_loader'><i class='fa fa-refresh fa-5x fa-spin'></i><p>Please Wait...</p></div>").fadeIn();
+           },
+           success: function(data) {
+               var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+               if (width <= 768) {
+                   $(".size-sm").css("display", "none");
+                   $(".size-sm-btn").css("display", "block");
+               }
+               setTimeout(function(){
+                   place.html(data);// fade in response data
+               }, 20);
+               setTimeout(function(){
+                   place.fadeIn(); // fade in response data
+               }, 30);
+           },
+           error: function(e) {
+               console.log(e)
+           }
+       });
+   }
+    function adv_search(){
+        $(".size-sm").css("display", "block");
+        $(".size-sm-btn").css("display", "none");
+    }
+
+    $(".s_country").change(function(){
+        var country_id = $(".s_country").val();
+        if (country_id == "") {
+            $(".s_state").html("<option value=''><?php echo translate('choose_a_country_first')?></option>");
+            $(".s_city").html("<option value=''><?php echo translate('choose_a_state_first')?></option>");
+        } else {
+            $.ajax({
+                url: "<?=base_url()?>home/get_dropdown_by_id/state/country_id/"+country_id, // form action url
+                type: 'POST', // form submit method get/post
+                dataType: 'html', // request type html/json/xml
+                cache       : false,
+                contentType : false,
+                processData : false,
+                success: function(data) {
+                    $(".s_state").html(data);
+                    $(".s_city").html("<option value=''><?php echo translate('choose_a_state_first')?></option>");
+                },
+                error: function(e) {
+                    console.log(e)
+                }
+            });
+        }
+    });
+
+    $(".s_state").change(function(){
+        var state_id = $(".s_state").val();
+        if (state_id == "") {
+            $(".s_city").html("<option value=''><?php echo translate('choose_a_state_first')?></option>");
+        } else {
+            $.ajax({
+                url: "<?=base_url()?>home/get_dropdown_by_id/city/state_id/"+state_id, // form action url
+                type: 'POST', // form submit method get/post
+                dataType: 'html', // request type html/json/xml
+                cache       : false,
+                contentType : false,
+                processData : false,
+                success: function(data) {
+                    $(".s_city").html(data);
+                },
+                error: function(e) {
+                    console.log(e)
+                }
+            });
+        }
+    });
+
+    $(".s_religion").change(function(){
         var religion_id = $(".s_religion").val();
         if (religion_id == "") {
             $(".s_caste").html("<option value=''><?php echo translate('choose_a_religion_first')?></option>");
@@ -96,6 +249,7 @@
                 contentType : false,
                 processData : false,
                 success: function(data) {
+
                     $(".s_caste").html(data);
                     $(".s_sub_caste").html("<option value=''><?php echo translate('choose_a_caste_first')?></option>");
                 },
@@ -124,7 +278,7 @@
                     } else {
                         $(".s_sub_caste").html(data);
                     };
-               },
+                },
                 error: function(e) {
                     console.log(e)
                 }
@@ -132,3 +286,39 @@
         }
     });
 </script>
+<style>
+    /* xs */
+    .size-sm {
+        display: none;
+    }
+    .size-sm-btn {
+        display: block;
+    }
+    /* sm */
+    @media (min-width: 768px) {
+        .size-sm {
+            display: none;
+        }
+        .size-sm-btn {
+            display: block;
+        }
+    }
+    /* md */
+    @media (min-width: 992px) {
+        .size-sm {
+            display: block;
+        }
+        .size-sm-btn {
+            display: none;
+        }
+    }
+    /* lg */
+    @media (min-width: 1200px) {
+        .size-sm {
+            display: block;
+        }
+        .size-sm-btn {
+            display: none;
+        }
+    }
+</style>
